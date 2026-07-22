f58dcaebe81c6eccc396dd400d7fef3235dcdbab
defnot67kid-beep <defnot67kid@gmail.com>
2026-07-22 11:05:25 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -37,7 +37,7 @@ else:
     client = pymongo.MongoClient(MONGO_URI)
     db = client["vodevs_bot_data"]
     levels_collection = db["levels"]
-    invites_collection = db["invites"]
+    invites_collection = db["admin_invites"]
     configs_collection = db["config"]
 
 # ==========================================
